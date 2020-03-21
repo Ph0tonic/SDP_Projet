4df00ba5026e59fb8623a21109b3b2983e5d220a
Bastien Wermeille <bastien.wermeille@gmail.com>
2020-03-21 18:20:45 +0100

    Remove deprecated methods call

--- app/src/main/java/ch/epfl/sdp/ui/home/HomeFragment.kt
+++ app/src/main/java/ch/epfl/sdp/ui/home/HomeFragment.kt
@@ -9,7 +9,7 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.ViewModelProvider
 import ch.epfl.sdp.R
 import ch.epfl.sdp.ui.maps.SupportMapFragmentActivity
 
@@ -22,8 +22,7 @@ class HomeFragment : Fragment() {
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel::class.java)
+        homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
         val textView: TextView = root.findViewById(R.id.text_home)
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
