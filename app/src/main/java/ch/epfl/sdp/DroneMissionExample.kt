0f02b76e348311fc6546c44fd0752c5237cf335d
AlexisTabin <alexis.tabin@gmail.com>
2020-03-25 14:32:39 +0100

    make functions public

--- app/src/main/java/ch/epfl/sdp/DroneMissionExample.kt
+++ app/src/main/java/ch/epfl/sdp/DroneMissionExample.kt
@@ -24,7 +24,7 @@ object DroneMissionExample {
         drone.action.arm().andThen(drone.action.takeoff()).subscribe()
     }
 
-    private fun generateMissionItem(latitudeDeg: Double, longitudeDeg: Double): Mission.MissionItem {
+    fun generateMissionItem(latitudeDeg: Double, longitudeDeg: Double): Mission.MissionItem {
         return Mission.MissionItem(
                 latitudeDeg,
                 longitudeDeg,
@@ -35,7 +35,7 @@ object DroneMissionExample {
                 1.0)
     }
 
-    private fun addMissionItems() {
+    fun addMissionItems() {
         missionItems.add(generateMissionItem(47.398039859999997, 8.5455725400000002))
         missionItems.add(generateMissionItem(47.398036222362471, 8.5450146439425509))
         missionItems.add(generateMissionItem(47.397825620791885, 8.5450092830163271))
