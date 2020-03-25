389aa8d683b549df7028f3e0e8c96bce71fd0840
AlexisTabin <alexis.tabin@gmail.com>
2020-03-25 18:40:54 +0100

    Change default port

--- app/src/main/java/ch/epfl/sdp/Drone.kt
+++ app/src/main/java/ch/epfl/sdp/Drone.kt
@@ -4,6 +4,6 @@ import io.mavsdk.System
 
 object Drone {
     private val BACKEND_IP_ADDRESS = "10.0.2.2" //emulator default address
-    private const val PORT = 50020
+    private const val PORT = 50051
     val instance : System = System(BACKEND_IP_ADDRESS, PORT)
 }
