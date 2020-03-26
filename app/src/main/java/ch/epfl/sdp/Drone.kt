9b15db738dd36c48ce242ee795a3384d4c4ba9ad
Dorian <doriansporefan@gmail.com>
2020-03-27 00:07:12 +0100

    Retrigger

--- app/src/main/java/ch/epfl/sdp/Drone.kt
+++ app/src/main/java/ch/epfl/sdp/Drone.kt
@@ -6,5 +6,4 @@ object Drone {
     private val BACKEND_IP_ADDRESS = "10.0.2.2" //emulator default address
     private const val PORT = 50051
     val instance : System = System(BACKEND_IP_ADDRESS, PORT)
-
 }
