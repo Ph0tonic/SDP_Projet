d0b38c692c6f4042577fd9670e8fdcc197211296
Sporarum <quentin.bernet@bluewin.ch>
2020-03-26 23:19:31 +0100

    Refactor var?-s into lateinit-s

--- app/src/main/java/ch/epfl/sdp/CentralLocationManager.kt
+++ app/src/main/java/ch/epfl/sdp/CentralLocationManager.kt
@@ -18,20 +18,19 @@ import androidx.core.content.ContextCompat.getSystemService
 import com.google.android.gms.tasks.OnFailureListener
 
 object CentralLocationManager {
-    private var locationManager: LocationManager? = null
-    private var activity: Activity? = null
-    private var locationDisabledAlert: AlertDialog.Builder? = null
+    private lateinit var locationManager: LocationManager
+    private lateinit var activity: Activity
+    private lateinit var locationDisabledAlert: AlertDialog.Builder
     private const val requestCode = 1011
 
 
     fun configure(activity: Activity) {
         this.activity = activity
-        Log.d("-----------------------", "Configuring: $activity")
-        locationManager = this.activity?.getSystemService(Context.LOCATION_SERVICE) as LocationManager?
+        locationManager = this.activity.getSystemService(Context.LOCATION_SERVICE) as LocationManager
 
 
-        locationDisabledAlert = AlertDialog.Builder(CentralLocationManager.activity)
-        locationDisabledAlert?.setTitle("Enable Location")
+        locationDisabledAlert = AlertDialog.Builder(this.activity)
+        locationDisabledAlert.setTitle("Enable Location")
                 ?.setMessage("This part of the app cannot function without location, please enable it")
                 ?.setPositiveButton("Location Settings") { paramDialogInterface, paramInt ->
                     val myIntent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
@@ -40,24 +39,21 @@ object CentralLocationManager {
                 ?.setNegativeButton("Cancel", DialogInterface.OnClickListener { paramDialogInterface, paramInt ->})
 
         if(checkPermission()){
-            locationManager?.requestLocationUpdates(
+            locationManager.requestLocationUpdates(
                     LocationManager.GPS_PROVIDER, 2 * 1000, 10f, CentralLocationListener)
         }
     }
 
     fun checkLocationSetting(): Boolean {
         if (!isLocationEnabled()){
-            locationDisabledAlert?.show()
+            locationDisabledAlert.show()
         }
         return isLocationEnabled()
     }
 
 
     private fun checkPermission(): Boolean {
-        return if(activity == null) {
-            Log.e("-----------------------","Activity null !")
-            false
-        } else if (ActivityCompat.checkSelfPermission(activity!!.applicationContext,Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(activity!!.applicationContext,Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        return if (ActivityCompat.checkSelfPermission(activity.applicationContext,Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(activity!!.applicationContext,Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             requestPermissions()
             false
         } else {
@@ -66,17 +62,17 @@ object CentralLocationManager {
     }
 
     private fun requestPermissions(){
-        ActivityCompat.requestPermissions(activity!!,arrayOf(Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION), requestCode)
+        ActivityCompat.requestPermissions(activity,arrayOf(Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION), requestCode)
     }
 
     private fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         if(requestCode == this.requestCode && checkPermission()){
-            locationManager?.requestLocationUpdates(
+            locationManager.requestLocationUpdates(
                     LocationManager.GPS_PROVIDER, 2 * 1000, 10f, CentralLocationListener)
         }
     }
 
     private fun isLocationEnabled(): Boolean {
-        return locationManager?.isProviderEnabled(LocationManager.GPS_PROVIDER) ?: false
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ?: false
     }
 }
