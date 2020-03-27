487c15641b35183faa2dd5d9e0dbfac2da5fa154
AlexisTabin <alexis.tabin@gmail.com>
2020-03-27 17:41:17 +0100

    Implemented simple example

--- app/src/main/java/ch/epfl/sdp/OfflineMapManagingActivity.kt
+++ app/src/main/java/ch/epfl/sdp/OfflineMapManagingActivity.kt
@@ -1,153 +1,196 @@
 package ch.epfl.sdp
 
+import ch.epfl.sdp.R
 import android.os.Bundle
-import android.util.Log
 import android.view.View
-import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.preference.PreferenceManager
 import com.mapbox.mapboxsdk.Mapbox
-import com.mapbox.mapboxsdk.camera.CameraPosition
 import com.mapbox.mapboxsdk.geometry.LatLng
 import com.mapbox.mapboxsdk.geometry.LatLngBounds
 import com.mapbox.mapboxsdk.maps.MapView
-import com.mapbox.mapboxsdk.maps.MapboxMap
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
 import com.mapbox.mapboxsdk.maps.Style
-import com.mapbox.mapboxsdk.offline.OfflineManager
-import com.mapbox.mapboxsdk.offline.OfflineTilePyramidRegionDefinition
+import com.mapbox.mapboxsdk.offline.*
+import com.mapbox.mapboxsdk.offline.OfflineManager.CreateOfflineRegionCallback
+import com.mapbox.mapboxsdk.offline.OfflineManager.ListOfflineRegionsCallback
+import com.mapbox.mapboxsdk.offline.OfflineRegion.OfflineRegionDeleteCallback
+import com.mapbox.mapboxsdk.offline.OfflineRegion.OfflineRegionObserver
 import org.json.JSONObject
+import timber.log.Timber
 
-class OfflineMapManagingActivity : AppCompatActivity(), OnMapReadyCallback {
-
+/**
+ * Download and view an offline map using the Mapbox Android SDK.
+ */
+class OfflineMapManagingActivity : AppCompatActivity() {
+    private var isEndNotified = false
+    private var progressBar: ProgressBar? = null
     private var mapView: MapView? = null
-    private var mapboxMap: MapboxMap? = null
-
+    private var offlineManager: OfflineManager? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         // Mapbox access token is configured here. This needs to be called either in your application
         // object or in the same activity which contains the mapview.
-        Mapbox.getInstance(this, getString(R.string.mapbox_access_token))
-
+        Mapbox.getInstance(this, getString(ch.epfl.sdp.R.string.mapbox_access_token))
+        // This contains the MapView in XML and needs to be called after the access token is configured.
         setContentView(R.layout.activity_offline_map)
-
         mapView = findViewById(R.id.store_mapoffline_mapView)
         mapView?.onCreate(savedInstanceState)
-        mapView?.getMapAsync(this)
+        mapView?.getMapAsync(OnMapReadyCallback { mapboxMap ->
+            mapboxMap.setStyle(Style.OUTDOORS) { style ->
+                // Set up the OfflineManager
+                offlineManager = OfflineManager.getInstance(this@OfflineMapManagingActivity)
+                // Create a bounding box for the offline region
+                val latLngBounds = LatLngBounds.Builder()
+                        .include(LatLng(37.7897, -119.5073)) // Northeast
+                        .include(LatLng(37.6744, -119.6815)) // Southwest
+                        .build()
+                // Define the offline region
+                val definition = OfflineTilePyramidRegionDefinition(
+                        style.uri,
+                        latLngBounds,
+                        10.0,
+                        20.0,
+                        this@OfflineMapManagingActivity.resources.displayMetrics.density)
+                // Set the metadata
+                val metadata: ByteArray?
+                metadata = try {
+                    val jsonObject = JSONObject()
+                    jsonObject.put(JSON_FIELD_REGION_NAME, "Yosemite National Park")
+                    val json = jsonObject.toString()
+                    json.toByteArray(charset(JSON_CHARSET))
+                } catch (exception: Exception) {
+                    Timber.e("Failed to encode metadata: %s", exception.message)
+                    null
+                }
+                // Create the region asynchronously
+                if (metadata != null) {
+                    offlineManager?.createOfflineRegion(
+                            definition,
+                            metadata,
+                            object : CreateOfflineRegionCallback {
+                                override fun onCreate(offlineRegion: OfflineRegion) {
+                                    offlineRegion.setDownloadState(OfflineRegion.STATE_ACTIVE)
+                                    // Display the download progress bar
+                                    progressBar = findViewById(R.id.progress_bar)
+                                    startProgress()
+                                    // Monitor the download progress using setObserver
+                                    offlineRegion.setObserver(object : OfflineRegionObserver {
+                                        override fun onStatusChanged(status: OfflineRegionStatus) { // Calculate the download percentage and update the progress bar
+                                            val percentage = if (status.requiredResourceCount >= 0) 100.0 * status.completedResourceCount / status.requiredResourceCount else 0.0
+                                            if (status.isRequiredResourceCountPrecise) { // Switch to determinate state
+                                                setPercentage(Math.round(percentage).toInt())
+                                            }
+                                        }
 
+                                        override fun onError(error: OfflineRegionError) { // If an error occurs, print to logcat
+                                            Timber.e("onError reason: %s", error.reason)
+                                            Timber.e("onError message: %s", error.message)
+                                        }
+
+                                        override fun mapboxTileCountLimitExceeded(limit: Long) { // Notify if offline region exceeds maximum tile count
+                                            Timber.e("Mapbox tile count limit exceeded: %s", limit)
+                                        }
+                                    })
+                                }
+
+                                override fun onError(error: String) {
+                                    Timber.e("Error: %s", error)
+                                }
+                            })
+                }
+            }
+        })
     }
 
-    fun downloadOfflineMap(view : View){
-        val longitude = mapboxMap?.cameraPosition?.target!!.longitude
-        val latitude = mapboxMap?.cameraPosition?.target!!.latitude
-
-        // Set up the OfflineManager
-        val offlineManager = OfflineManager.getInstance(this)
-
-        // Create a bounding box for the offline region
-        val latLngBounds = LatLngBounds.Builder()
-                .include(LatLng(latitude + 0.1, longitude + 0.1)) // Northeast
-                .include(LatLng(latitude - 0.1, longitude - 0.1)) // Southwest
-                .build()
-
-        // Define the offline region
-        val definition = OfflineTilePyramidRegionDefinition(
-                Style.SATELLITE,
-                latLngBounds,
-                10.0,
-                20.0,this.resources.displayMetrics.density)
-
-        // Implementation that uses JSON to store a map as the offline region name.
-        var metadata: ByteArray?
-        try {
-            val jsonObject = JSONObject()
-            val JSON_FIELD_REGION_NAME = "test"
-            jsonObject.put(JSON_FIELD_REGION_NAME, "Yosemite National Park")
-            val json = jsonObject.toString()
-            val JSON_CHARSET = "test_name"
-            metadata = json.toByteArray(charset(JSON_CHARSET))
-        } catch (exception: Exception) {
-            Log.e("---------------> TAG", "Failed to encode metadata: " + exception.message)
-            metadata = null
-        }
-
-
-    }
-
-    override fun onMapReady(mapboxMap: MapboxMap) {
-        this.mapboxMap = mapboxMap
-        mapboxMap.setStyle(Style.MAPBOX_STREETS)
-
-        val mapNumber = intent.getIntExtra("ButtonId", 0)
-        var latitude : Double = -52.6885
-        var longitude : Double = -70.1395
-        var zoom : Double = 9.0
-
-        when(mapNumber) {
-            0 -> {
-                // Load latest location
-                val latitude: Double = PreferenceManager.getDefaultSharedPreferences(this)
-                        .getString("latitude", null)?.toDouble() ?: -52.6885
-                val longitude: Double = PreferenceManager.getDefaultSharedPreferences(this)
-                        .getString("longitude", null)?.toDouble() ?: -70.1395
-                val zoom: Double = PreferenceManager.getDefaultSharedPreferences(this)
-                        .getString("zoom", null)?.toDouble() ?: 9.0
-            }
-            1 -> {
-                latitude = 45.980537
-                longitude = 7.641618
-            }
-            2 -> {
-                latitude = 45.832622
-                longitude = 6.865175
-            }
-            3 -> {
-                latitude = 48.858093
-                longitude = 2.294694
-            }
-        }
-
-        mapboxMap.cameraPosition = CameraPosition.Builder()
-                .target(LatLng(latitude, longitude))
-                .zoom(zoom)
-                .build()
-
+    public override fun onResume() {
+        super.onResume()
+        mapView!!.onResume()
     }
 
     override fun onStart() {
         super.onStart()
-        mapView?.onStart()
-    }
-
-    override fun onResume() {
-        super.onResume()
-        mapView?.onResume()
-    }
-
-    override fun onPause() {
-        super.onPause()
-        mapView?.onPause()
+        mapView!!.onStart()
     }
 
     override fun onStop() {
         super.onStop()
-        mapView?.onStop()
+        mapView!!.onStop()
     }
 
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        mapView?.onSaveInstanceState(outState)
+    public override fun onPause() {
+        super.onPause()
+        mapView!!.onPause()
+        if (offlineManager != null) {
+            offlineManager!!.listOfflineRegions(object : ListOfflineRegionsCallback {
+                override fun onList(offlineRegions: Array<OfflineRegion>) {
+                    if (offlineRegions.size > 0) { // delete the last item in the offlineRegions list which will be yosemite offline map
+                        offlineRegions[offlineRegions.size - 1].delete(object : OfflineRegionDeleteCallback {
+                            override fun onDelete() {
+                                Toast.makeText(
+                                        this@OfflineMapManagingActivity,
+                                        "TESSSSSST",
+                                        Toast.LENGTH_LONG
+                                ).show()
+                            }
+
+                            override fun onError(error: String) {
+                                Timber.e("On delete error: %s", error)
+                            }
+                        })
+                    }
+                }
+
+                override fun onError(error: String) {
+                    Timber.e("onListError: %s", error)
+                }
+            })
+        }
     }
 
     override fun onLowMemory() {
         super.onLowMemory()
-        mapView?.onLowMemory()
+        mapView!!.onLowMemory()
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        mapView?.onDestroy()
+        mapView!!.onDestroy()
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView!!.onSaveInstanceState(outState)
+    }
+
+    // Progress bar methods
+    private fun startProgress() { // Start and show the progress bar
+        isEndNotified = false
+        progressBar!!.isIndeterminate = true
+        progressBar!!.visibility = View.VISIBLE
+    }
+
+    private fun setPercentage(percentage: Int) {
+        progressBar!!.isIndeterminate = false
+        progressBar!!.progress = percentage
+    }
+
+    private fun endProgress(message: String) { // Don't notify more than once
+        if (isEndNotified) {
+            return
+        }
+        // Stop and hide the progress bar
+        isEndNotified = true
+        progressBar!!.isIndeterminate = false
+        progressBar!!.visibility = View.GONE
+        // Show a toast
+        Toast.makeText(this@OfflineMapManagingActivity, message, Toast.LENGTH_LONG).show()
+    }
+
+    companion object {
+        // JSON encoding/decoding
+        const val JSON_CHARSET = "UTF-8"
+        const val JSON_FIELD_REGION_NAME = "FIELD_REGION_NAME"
+    }
 }
