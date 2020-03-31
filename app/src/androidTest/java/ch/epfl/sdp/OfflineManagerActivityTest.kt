c6659f12e3fe671a40127d0af6f1b96328f4afde
AlexisTabin <alexis.tabin@gmail.com>
2020-03-31 20:45:44 +0200

    download a very tiny map

--- app/src/androidTest/java/ch/epfl/sdp/OfflineManagerActivityTest.kt
+++ app/src/androidTest/java/ch/epfl/sdp/OfflineManagerActivityTest.kt
@@ -1,12 +1,6 @@
 package ch.epfl.sdp
 
-import android.app.Activity
 import android.content.Context
-import android.view.View
-import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.EditText
-import androidx.core.content.ContextCompat.getSystemService
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.typeText
@@ -15,27 +9,30 @@ import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.internal.runner.junit4.statement.UiThreadStatement.runOnUiThread
 import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
 import androidx.test.rule.ActivityTestRule
 import androidx.test.uiautomator.By
 import androidx.test.uiautomator.UiDevice
 import androidx.test.uiautomator.Until
-import ch.epfl.sdp.R.string.dialog_title
 import com.mapbox.mapboxsdk.camera.CameraPosition
 import com.mapbox.mapboxsdk.geometry.LatLng
 import kotlinx.android.synthetic.main.activity_map.*
 import org.junit.*
 import org.junit.runner.RunWith
-import org.junit.runners.MethodSorters
 
 
 @RunWith(AndroidJUnit4::class)
 class OfflineManagerActivityTest {
     private var mUiDevice: UiDevice? = null
-    private val latitude = 46.307165438
-    private val longitude = 7.476331
+
+    private val cmalatitude = 46.307165438
+    private val cmalongitude = 7.476331
     private val zoom = 10.0
     private val name = "Crans-Montana"
+
+    private val oceanlatitude =  46.399111
+    private val oceanlongitude = -31.697953
+    private val oceanName = "Pacifique"
+    private val oceanZoom = 19.0
     private val timeout : Long = 2000
 
     @Before
@@ -45,8 +42,8 @@ class OfflineManagerActivityTest {
         runOnUiThread {
             mActivityRule.activity.mapView.getMapAsync { mapboxMap ->
                 mapboxMap.cameraPosition = CameraPosition.Builder()
-                        .target(LatLng(latitude, longitude))
-                        .zoom(zoom)
+                        .target(LatLng(oceanlatitude, oceanlongitude))
+                        .zoom(oceanZoom)
                         .build()
             }
         }
@@ -75,7 +72,7 @@ class OfflineManagerActivityTest {
 
     @Test
     fun canOpenListDialog(){
-        mUiDevice?.wait(Until.hasObject(By.desc("List").clickable(true)), timeout)
+        mUiDevice?.wait(Until.hasObject(By.desc("List")), timeout)
         onView(withId(R.id.list_button)).perform(click())
 
         mUiDevice?.wait(Until.hasObject(By.desc("List")), timeout)
@@ -85,10 +82,10 @@ class OfflineManagerActivityTest {
 
     @Test
     fun canCancelDownload(){
-        mUiDevice?.wait(Until.hasObject(By.desc("Download").clickable(true)), timeout)
+        mUiDevice?.wait(Until.hasObject(By.desc("Download")), timeout)
         onView(withText(R.string.dialog_positive_button)).perform(click())
 
-        mUiDevice?.wait(Until.hasObject(By.desc("CANCEL").clickable(true)), timeout)
+        mUiDevice?.wait(Until.hasObject(By.desc("CANCEL")), timeout)
         onView(withText(R.string.dialog_negative_button)).perform(click())
     }
 
@@ -111,10 +108,34 @@ class OfflineManagerActivityTest {
 
         mUiDevice?.pressBack()
 
-        mUiDevice?.wait(Until.hasObject(By.desc("DOWNLOAD").clickable(true)), timeout)
+        mUiDevice?.wait(Until.hasObject(By.desc("DOWNLOAD")), timeout)
         onView(withText(R.string.dialog_positive_button)).perform(click())
     }
 
+    @Test
+    fun canDeleteOceanMap(){
+
+        mUiDevice?.wait(Until.hasObject(By.desc("MAP READY")), timeout)
+        onView(withText(R.string.dialog_positive_button)).perform(click())
+
+        mUiDevice?.wait(Until.hasObject(By.desc("Enter")), timeout)
+        onView(withId(R.id.dialog_textfield_id)).perform(typeText(oceanName))
+
+        mUiDevice?.pressBack()
+
+        mUiDevice?.wait(Until.hasObject(By.desc("DOWNLOAD")), timeout)
+        onView(withText(R.string.dialog_positive_button)).perform(click())
+
+        Thread.sleep(timeout)
+
+        mUiDevice?.wait(Until.hasObject(By.desc("List")),  timeout)
+        onView(withId(R.id.list_button)).perform(click())
+
+        mUiDevice?.wait(Until.hasObject(By.desc("DELETE")), timeout)
+        onView(withText(R.string.navigate_neutral_button_title)).perform(click())
+    }
+
+    /*
     @Test
     fun canDeleteMap(){
 
@@ -135,22 +156,23 @@ class OfflineManagerActivityTest {
         mUiDevice?.wait(Until.hasObject(By.desc("DELETE").clickable(true)), timeout)
         onView(withText(R.string.navigate_neutral_button_title)).perform(click())
     }
+     */
 
     @Test
     fun canNavigateTo(){
-        mUiDevice?.wait(Until.hasObject(By.desc("List").clickable(true)), timeout)
+        mUiDevice?.wait(Until.hasObject(By.desc("List")), timeout)
         onView(withId(R.id.list_button)).perform(click())
 
-        mUiDevice?.wait(Until.hasObject(By.desc("NAVIGATE").clickable(true)), timeout)
+        mUiDevice?.wait(Until.hasObject(By.desc("NAVIGATE")), timeout)
         onView(withText(R.string.navigate_positive_button)).perform(click())
     }
 
     @Test
     fun cannotEmptyDownloadName(){
-        mUiDevice?.wait(Until.hasObject(By.desc("DOWNLOAD").clickable(true)), timeout)
+        mUiDevice?.wait(Until.hasObject(By.desc("DOWNLOAD")), timeout)
         onView(withText(R.string.dialog_positive_button)).perform(click())
 
-        mUiDevice?.wait(Until.hasObject(By.desc("DOWNLOAD").clickable(true)), timeout)
+        mUiDevice?.wait(Until.hasObject(By.desc("DOWNLOAD")), timeout)
         onView(withText(R.string.dialog_positive_button)).perform(click())
     }
 }
