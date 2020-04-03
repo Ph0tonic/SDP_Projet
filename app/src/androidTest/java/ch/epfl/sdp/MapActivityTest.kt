6516558cf5b5336edd0daee1d3f1bc0e18a0bb76
Bastien Wermeille <bastien.wermeille@gmail.com>
2020-04-03 08:43:54 +0200

    Fix missed merge conflict

--- app/src/androidTest/java/ch/epfl/sdp/MapActivityTest.kt
+++ app/src/androidTest/java/ch/epfl/sdp/MapActivityTest.kt
@@ -7,11 +7,6 @@ import android.content.SharedPreferences
 import androidx.preference.PreferenceManager
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
-<<<<<<< HEAD
-=======
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.action.ViewActions.doubleClick
->>>>>>> a870bbc8d49f6000886ff6fd087d8b97f01589a7
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.internal.runner.junit4.statement.UiThreadStatement.runOnUiThread
