d89cc32ca03ac486fe1379f0dc3bb17d781668df
Bastien Wermeille <bastien.wermeille@gmail.com>
2020-04-04 11:20:52 +0200

    Refactor some unitTest with functionnal programming

--- app/src/test/java/ch/epfl/sdp/DroneMissionTest.kt
+++ app/src/test/java/ch/epfl/sdp/DroneMissionTest.kt
@@ -4,19 +4,17 @@ import com.mapbox.mapboxsdk.geometry.LatLng
 import io.mavsdk.mission.Mission
 import org.junit.Assert
 import org.junit.Test
-import java.util.ArrayList
 import java.util.concurrent.ThreadLocalRandom
-import ch.epfl.sdp.DroneMission as DME
 
 class DroneMissionTest {
 
     @Test
-    fun generateMissionItemTest(){
+    fun generateMissionItemTest() {
         val n = 100
         for (x in 0..n) {
             val randomLatitude = ThreadLocalRandom.current().nextDouble(-90.0, 90.0)
             val randomLongitude = ThreadLocalRandom.current().nextDouble(0.0, 180.0)
-            var mission = Mission.MissionItem(
+            val mission = Mission.MissionItem(
                     randomLatitude,
                     randomLongitude,
                     10f,
@@ -24,33 +22,34 @@ class DroneMissionTest {
                     true, Float.NaN, Float.NaN,
                     Mission.MissionItem.CameraAction.NONE, Float.NaN,
                     1.0)
-            var expectedMission = DME.generateMissionItem(randomLatitude, randomLongitude)
+            val expectedMission = DroneMission.generateMissionItem(randomLatitude, randomLongitude)
             Assert.assertTrue(missionEquality(expectedMission, mission))
         }
     }
 
     @Test
-    fun makeDroneMissionTest(){
-        val positions = arrayListOf<LatLng>()
-        positions.add(LatLng(47.398039859999997, 8.5455725400000002))
-        positions.add(LatLng(47.398036222362471, 8.5450146439425509))
-        positions.add(LatLng(47.397825620791885, 8.5450092830163271))
-        positions.add(LatLng(47.397832880000003, 8.5455939999999995))
+    fun makeDroneMissionTest() {
+        val positions = arrayListOf(
+                LatLng(47.398039859999997, 8.5455725400000002),
+                LatLng(47.398036222362471, 8.5450146439425509),
+                LatLng(47.397825620791885, 8.5450092830163271),
+                LatLng(47.397832880000003, 8.5455939999999995)
+        )
 
-        val expectedMissionItems: ArrayList<Mission.MissionItem> = arrayListOf<Mission.MissionItem>()
-        for(pos in positions){
-            expectedMissionItems.add(ch.epfl.sdp.DroneMission.generateMissionItem(pos.latitude, pos.longitude))
+        val expectedMissionItems = positions.map { pos ->
+            DroneMission.generateMissionItem(pos.latitude, pos.longitude)
         }
 
-        val droneMissionExample =  DME.makeDroneMission(positions)
-        val missionsItems = droneMissionExample.getMissionItems()
+        val missionsItems = DroneMission
+                .makeDroneMission(positions)
+                .getMissionItems()
 
-        for((i, expectedMission) in expectedMissionItems.withIndex()){
-            Assert.assertTrue(missionEquality(expectedMission, missionsItems[i]))
+        expectedMissionItems.zip(missionsItems).forEach { (expected, observed) ->
+            Assert.assertTrue(missionEquality(expected, observed))
         }
     }
 
-    fun missionEquality (m1 : Mission.MissionItem, m2 : Mission.MissionItem) : Boolean{
+    private fun missionEquality(m1: Mission.MissionItem, m2: Mission.MissionItem): Boolean {
         val lat = m1.latitudeDeg.equals(m2.latitudeDeg)
         val lon = m1.longitudeDeg.equals(m2.longitudeDeg)
         val alt = m1.relativeAltitudeM.equals(m2.relativeAltitudeM)
