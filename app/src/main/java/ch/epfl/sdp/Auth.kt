8c6de7ed800191d3c902fbdb51518618363be664
Bastien Wermeille <bastien.wermeille@gmail.com>
2020-04-05 21:06:25 +0200

    Add MainApplication class and create Auth object

---
+++ app/src/main/java/ch/epfl/sdp/Auth.kt
@@ -0,0 +1,38 @@
+package ch.epfl.sdp
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+
+object Auth : ViewModel() {
+
+    val loggedIn: MutableLiveData<Boolean> by lazy {
+        MutableLiveData<Boolean>()
+    }
+    val email: MutableLiveData<String> by lazy {
+        MutableLiveData<String>()
+    }
+    val profileImageURL: MutableLiveData<String> by lazy {
+        MutableLiveData<String>()
+    }
+    val name: MutableLiveData<String> by lazy {
+        MutableLiveData<String>()
+    }
+
+    init {
+        GoogleSignIn.getLastSignedInAccount(MainApplication.applicationContext())
+                .takeIf { account ->
+                    Log.e("DEBUG", "Filter logged in")
+                    account != null
+                }
+                .run {
+                    email.postValue(this?.email)
+                    name.postValue(this?.displayName)
+                    profileImageURL.postValue(this?.photoUrl.toString())
+                    loggedIn.postValue(true)
+                    Log.e("DEBUG", "logged in")
+                }
+        Log.e("DEBUG", "INIT")
+    }
+}
---
+++ app/src/main/java/ch/epfl/sdp/ui/home/LoginNavFragment.kt
@@ -0,0 +1,88 @@
+package ch.epfl.sdp.ui.home
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import ch.epfl.sdp.R
+import com.bumptech.glide.Glide
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+
+class LoginNavFragment : Fragment() {
+
+    private lateinit var mGoogleSignInClient: GoogleSignInClient
+
+    companion object {
+        private const val RC_SIGN_IN = 9001
+    }
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        val root = inflater.inflate(R.layout.fragment_login_nav, container, false)
+
+        Log.e("DEBUG", "Set listener")
+
+        root.findViewById<TextView>(R.id.nav_login_button).setOnClickListener {
+            startActivityForResult(mGoogleSignInClient.signInIntent, RC_SIGN_IN)
+        }
+        Auth.email.observe(viewLifecycleOwner, Observer { email ->
+            root.findViewById<TextView>(R.id.nav_username).text = email
+        })
+        Auth.name.observe(viewLifecycleOwner, Observer { name ->
+            root.findViewById<TextView>(R.id.nav_username).text = name
+        })
+        Auth.profileImageURL.observe(viewLifecycleOwner, Observer { imageURL ->
+            Glide
+                    .with(context)
+
+                    .load(imageURL)
+                    .error(R.mipmap.ic_launcher_round)
+                    .into(root.findViewById(R.id.nav_user_image))
+        })
+        Auth.loggedIn.observe(viewLifecycleOwner, Observer { loggedIn ->
+            val visibility = if (loggedIn) View.VISIBLE else View.GONE
+            root.findViewById<TextView>(R.id.nav_username).visibility = visibility
+            root.findViewById<TextView>(R.id.nav_user_email).visibility = visibility
+            root.findViewById<TextView>(R.id.nav_user_image).visibility = visibility
+            root.findViewById<TextView>(R.id.nav_login_button).visibility = if (loggedIn) View.VISIBLE else View.GONE
+        })
+        return root
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        val gso = GoogleSignInOptions
+                .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.google_signin_key))
+                .requestEmail()
+                .build()
+        mGoogleSignInClient = GoogleSignIn.getClient(context, gso)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN) {
+            try {
+                val account: GoogleSignInAccount? = GoogleSignIn.getSignedInAccountFromIntent(data).getResult(ApiException::class.java)
+                //updateUserView(account?.displayName, account?.email, account?.photoUrl.toString())
+            } catch (e: ApiException) {
+//                Snackbar.make(findViewById(R.id.main_nav_header), "Could not sign in :(", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show()
+            }
+        }
+    }
+}
