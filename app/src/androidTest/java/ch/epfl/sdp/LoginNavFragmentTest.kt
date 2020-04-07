b3d19a43ff9fd5608001a2057e34a1310d5b4718
Bastien Wermeille <bastien.wermeille@gmail.com>
2020-04-07 19:06:13 +0200

    Update test

--- app/src/androidTest/java/ch/epfl/sdp/LoginNavFragmentTest.kt
+++ app/src/androidTest/java/ch/epfl/sdp/LoginNavFragmentTest.kt
@@ -1,21 +1,26 @@
 package ch.epfl.sdp
 
 import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.content.Context
 import android.view.Gravity
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.DrawerActions
 import androidx.test.espresso.contrib.DrawerMatchers
+import androidx.test.espresso.contrib.NavigationViewActions
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.matcher.IntentMatchers
 import androidx.test.espresso.intent.rule.IntentsTestRule
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.internal.runner.junit4.statement.UiThreadStatement.runOnUiThread
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.rule.GrantPermissionRule
 import androidx.test.rule.GrantPermissionRule.grant
+import androidx.test.uiautomator.By
 import androidx.test.uiautomator.UiDevice
+import androidx.test.uiautomator.Until
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import org.junit.Before
@@ -27,6 +32,7 @@ import org.junit.runner.RunWith
 class LoginNavFragmentTest {
 
     companion object {
+        private const val TIMEOUT:Long = 2000
         private const val FAKE_NAME = "Fake Girl"
         private const val FAKE_EMAIL = "fake@fake.com"
         private const val FAKE_PROFILE_IMAGE_URL = "https://fakeimg.pl/80x80/"
@@ -56,13 +62,25 @@ class LoginNavFragmentTest {
                 .build()
     }
 
-    @Test
-    fun clickSignInShouldLaunchIntent() {
-        Auth.logout()
+    private fun getContext(): Context {
+        return InstrumentationRegistry.getInstrumentation().targetContext
+    }
+
+    private fun openDrawer() {
         onView(withId(R.id.drawer_layout))
                 .check(matches(DrawerMatchers.isClosed(Gravity.LEFT))) // Check that drawer is closed to begin with
                 .perform(DrawerActions.open())
+    }
 
+    @Test
+    fun clickSignInShouldLaunchIntent() {
+        runOnUiThread {
+            Auth.logout()
+            Auth.loggedIn.postValue(false)
+        }
+
+        openDrawer()
+        mUiDevice.wait(Until.hasObject(By.text(getContext().getString(R.string.auth_signin))), TIMEOUT)
         onView(withId(R.id.nav_signin_button)).check(matches(isClickable())).perform(click())
 
         val mGoogleSignInClient = GoogleSignIn.getClient(MainApplication.applicationContext(), getGSO())
@@ -72,24 +90,31 @@ class LoginNavFragmentTest {
 
     @Test
     fun whenAuthValuesAreUpdatedInterfaceShouldBeUpdated() {
-        Auth.logout()
-        onView(withId(R.id.drawer_layout))
-                .check(matches(DrawerMatchers.isClosed(Gravity.LEFT))) // Check that drawer is closed to begin with
-                .perform(DrawerActions.open())
+        runOnUiThread {
+            Auth.logout()
+            Auth.loggedIn.postValue(false)
+        }
 
-        Auth.email.postValue(FAKE_EMAIL)
-        Auth.name.postValue(FAKE_NAME)
-        Auth.profileImageURL.postValue(FAKE_PROFILE_IMAGE_URL)
-        Auth.loggedIn.postValue(true)
+        openDrawer()
+        runOnUiThread {
+            Auth.email.postValue(FAKE_EMAIL)
+            Auth.name.postValue(FAKE_NAME)
+            Auth.profileImageURL.postValue(FAKE_PROFILE_IMAGE_URL)
+            Auth.loggedIn.postValue(true)
+        }
 
         onView(withId(R.id.nav_username)).check(matches(withText(FAKE_NAME)))
         onView(withId(R.id.nav_user_email)).check(matches(withText(FAKE_EMAIL)))
         onView(withId(R.id.nav_user_image)).check(matches(isDisplayed()))
 
-        onView(withId(R.id.nav_signout_button)).check(matches(isDisplayed())).perform(click())
+        mUiDevice.wait(Until.hasObject(By.text(getContext().getString(R.string.auth_signout))), TIMEOUT)
+        onView(withId(R.id.nav_view))
+                .perform(NavigationViewActions.navigateTo(R.id.nav_signout_button))
+
+        openDrawer()
+        mUiDevice.wait(Until.hasObject(By.text(getContext().getString(R.string.auth_signin))), TIMEOUT)
         onView(withId(R.id.nav_signin_button)).check(matches(isDisplayed()))
 
         mUiDevice.pressBack()
     }
-
 }
