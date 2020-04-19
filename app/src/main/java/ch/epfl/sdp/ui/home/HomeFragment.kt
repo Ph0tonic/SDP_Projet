1052c06f06853a8315b8ac4fbbe3817bccd11dc0
unknown <julien.sahli@protonmail.ch>
2020-04-19 17:26:49 +0200

    Main activity redesigned, color scheme changed

--- app/src/main/java/ch/epfl/sdp/ui/home/HomeFragment.kt
+++ app/src/main/java/ch/epfl/sdp/ui/home/HomeFragment.kt
@@ -26,10 +26,6 @@ class HomeFragment : Fragment() {
     ): View? {
         homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
 
         root.findViewById<Button>(R.id.display_map).setOnClickListener {
             startActivity(Intent(context, MapActivity::class.java))
