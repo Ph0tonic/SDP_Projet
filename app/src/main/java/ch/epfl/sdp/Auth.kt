74f151d82062e07e5d25af9e59e337a2ee20e6b5
AlexisTabin <alexis.tabin@gmail.com>
2020-04-21 14:52:51 +0200

    fix import issue Auth.kt

--- app/src/main/java/ch/epfl/sdp/Auth.kt
+++ app/src/main/java/ch/epfl/sdp/Auth.kt
@@ -2,19 +2,16 @@ package ch.epfl.sdp
 
 import android.app.Activity
 import android.content.Intent
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-
-import ch.epfl.sdp.ui.maps.OfflineManagerUtils.showToast
-
+import ch.epfl.sdp.ui.maps.OfflineManagerUtils
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 
-object Auth : ViewModel() {
+object Auth : ViewModel(), OfflineManagerUtils {
 
     private const val RC_SIGN_IN = 9001
 
@@ -66,7 +63,7 @@ object Auth : ViewModel() {
         createGoogleSignClient().signOut().addOnSuccessListener {
             val context = MainApplication.applicationContext()
             loggedIn.postValue(false)
-            Toast.makeText(context, context.getString(R.string.sign_out_success), Toast.LENGTH_SHORT).show()
+            showToast(context.getString(R.string.sign_out_success))
         }
     }
 
