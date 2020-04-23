4ec079238b33f67a3137df5ca085c1ba458fb3b2
Nolan Chappuis <nolanchappuis@hotmail.com>
2020-04-23 14:57:58 +0200

    Remove clustered layer

--- app/src/main/java/ch/epfl/sdp/ui/maps/MapUtils.kt
+++ app/src/main/java/ch/epfl/sdp/ui/maps/MapUtils.kt
@@ -6,7 +6,6 @@ import ch.epfl.sdp.MainApplication
 import ch.epfl.sdp.R
 import com.mapbox.mapboxsdk.camera.CameraPosition
 import com.mapbox.mapboxsdk.geometry.LatLng
-import com.mapbox.mapboxsdk.maps.MapboxMap
 import com.mapbox.mapboxsdk.maps.Style
 import com.mapbox.mapboxsdk.style.expressions.Expression
 import com.mapbox.mapboxsdk.style.layers.CircleLayer
@@ -18,9 +17,6 @@ object MapUtils {
     const val DEFAULT_LONGITUDE: Double = 8.545970150745575
     const val DEFAULT_ZOOM: Double = 9.0
 
-    private val DARK_RED = Color.parseColor("#E55E5E")
-    private val LIGHT_RED = Color.parseColor("#F9886C")
-    private val ORANGE = Color.parseColor("#FBB03B")
 
     private fun loadLastMapPositionFromPrefs(): LatLng {
         val context = MainApplication.applicationContext()
@@ -86,7 +82,6 @@ object MapUtils {
      */
     fun createLayersForHeatMap(style: Style) {
         unclusteredLayerData(style)
-        //clusteredLayerData(style)
     }
 
     private fun unclusteredLayerData(style: Style) {
@@ -108,33 +103,5 @@ object MapUtils {
         unclustered.setFilter(Expression.neq(Expression.get("cluster"), Expression.literal(true)))
         style.addLayerBelow(unclustered,
                 MainApplication.applicationContext().getString(R.string.heatmap_source_ID))
-    }
-
-    private fun clusteredLayerData(style: Style) {
-        val layers = arrayOf(
-                intArrayOf(15, DARK_RED),
-                intArrayOf(10, LIGHT_RED),
-                intArrayOf(2, ORANGE))
-        layers.indices.forEach { i ->
-            val circles = CircleLayer("cluster-$i",
-                    MainApplication.applicationContext().getString(R.string.heatmap_source_ID))
-            circles.setProperties(
-                    PropertyFactory.circleColor(layers[i][1]),
-                    PropertyFactory.circleRadius(100f),
-                    PropertyFactory.circleBlur(1f)
-            )
-
-            val pointCount: Expression = Expression.toNumber(Expression.get("point_count"))
-            circles.setFilter(Expression.gte(pointCount,Expression.literal(1)))
-            circles.setFilter(
-                    if (i == 0) Expression.gte(pointCount, Expression.literal(layers[i][0]))
-                    else Expression.all(
-                            Expression.gte(pointCount, Expression.literal(layers[i][0])),
-                            Expression.lt(pointCount, Expression.literal(layers[i - 1][0]))
-                    )
-            )
-            style.addLayerBelow(circles,
-                    MainApplication.applicationContext().getString(R.string.heatmap_source_ID))
-        }
     }
 }
