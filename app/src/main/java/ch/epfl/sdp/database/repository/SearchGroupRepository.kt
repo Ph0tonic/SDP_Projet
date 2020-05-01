ae9146fe039bc3cebf4632e605be5843f8886f00
Bastien Wermeille <5320541+Ph0tonic@users.noreply.github.com>
2020-05-01 17:50:57 +0200

    Comment SearchGroupRepository

--- app/src/main/java/ch/epfl/sdp/database/repository/SearchGroupRepository.kt
+++ app/src/main/java/ch/epfl/sdp/database/repository/SearchGroupRepository.kt
@@ -1,24 +1,19 @@
 package ch.epfl.sdp.database.repository
 
-import androidx.lifecycle.MutableLiveData
-import ch.epfl.sdp.database.dao.FirebaseGroupDao
-import ch.epfl.sdp.database.dao.GroupDao
-import ch.epfl.sdp.database.data.SearchGroupData
-
-class SearchGroupRepository {
-    companion object {
-        val DEFAULT_DAO = { FirebaseGroupDao() }
-        // Change this for dependency injection
-        var daoProvider: () -> GroupDao = DEFAULT_DAO
-    }
-
-    val dao: GroupDao = daoProvider()
-
-    fun getGroups(): MutableLiveData<List<SearchGroupData>> {
-        return dao.getGroups()
-    }
-
-    fun getGroupById(groupId: String): MutableLiveData<SearchGroupData> {
-        return dao.getGroupById(groupId)
-    }
-}
+//class SearchGroupRepository {
+//    companion object {
+//        val DEFAULT_DAO = { FirebaseGroupDao() }
+//        // Change this for dependency injection
+//        var daoProvider: () -> GroupDao = DEFAULT_DAO
+//    }
+//
+//    val dao: GroupDao = daoProvider()
+//
+//    fun getGroups(): MutableLiveData<List<SearchGroupData>> {
+//        return dao.getGroups()
+//    }
+//
+//    fun getGroupById(groupId: String): MutableLiveData<SearchGroupData> {
+//        return dao.getGroupById(groupId)
+//    }
+//}
