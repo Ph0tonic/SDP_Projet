df7b1a963d61db9cb6667b726017fc9fa46de1d4
Dorian <doriansporefan@gmail.com>
2020-05-13 12:41:56 +0200

    Fix code location for getting users of a group

--- app/src/main/java/ch/epfl/sdp/SearchGroupEditionActivity.kt
+++ app/src/main/java/ch/epfl/sdp/SearchGroupEditionActivity.kt
@@ -12,7 +12,6 @@ import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import ch.epfl.sdp.database.data.SearchGroupData
-import ch.epfl.sdp.database.data.UserData
 import ch.epfl.sdp.database.repository.SearchGroupRepository
 import ch.epfl.sdp.ui.searchgroupselection.UserRecyclerAdapter
 
@@ -34,41 +33,26 @@ class SearchGroupEditionActivity : AppCompatActivity() {
         if (!createGroup) {
             findViewById<Button>(R.id.search_group_edition_create_or_save_button).text = getString(R.string.save_group_changes)
             groupRepo.getGroupById(groupId!!).observe(this, Observer {
-                if (it != null){
+                if (it != null) {
                     findViewById<TextView>(R.id.group_editor_group_name).text = it.name
                 }
             })
+
+            val operatorsRecyclerView = findViewById<RecyclerView>(R.id.group_edit_operator_recyclerview)
+            operatorsRecyclerView.layoutManager = LinearLayoutManager(this)
+
+            val rescuersRecyclerView = findViewById<RecyclerView>(R.id.group_edit_rescuer_recyclerview)
+            rescuersRecyclerView.layoutManager = LinearLayoutManager(this)
+
+            groupRepo.getOperatorsOfGroup(groupId!!).observe(this, Observer {
+                operatorsRecyclerView.adapter = UserRecyclerAdapter(it.toList())
+                Log.w("FIREBASE", "operators: $it")
+            })
+            
+            groupRepo.getRescuersOfGroup(groupId!!).observe(this, Observer {
+                rescuersRecyclerView.adapter = UserRecyclerAdapter(it.toList())
+            })
         }
-
-        val operatorsRecyclerView = findViewById<RecyclerView>(R.id.group_edit_operator_recyclerview)
-        operatorsRecyclerView.layoutManager = LinearLayoutManager(this)
-
-        val rescuersRecyclerView = findViewById<RecyclerView>(R.id.group_edit_rescuer_recyclerview)
-        rescuersRecyclerView.layoutManager = LinearLayoutManager(this)
-
-        //TODO get actual data from the database
-        val user1 = UserData()
-        val user2 = UserData()
-        val user3 = UserData()
-        val user4 = UserData()
-        val user5 = UserData()
-
-        user1.email="test1@gmail.com"
-        user2.email="test2@gmail.com"
-        user3.email="test3@gmail.com"
-        user4.email="test4@gmail.com"
-        user5.email="test5@gmail.com"
-
-        groupRepo.getOperatorsOfGroup(groupId!!).observe(this, Observer {
-            operatorsRecyclerView.adapter = UserRecyclerAdapter(it.toList())
-            Log.w("FIREBASE","operators: $it")
-        })
-
-        val rescuers = MutableLiveData(listOf(user3, user4, user5))
-
-        groupRepo.getRescuersOfGroup(groupId!!).observe(this, Observer {
-            rescuersRecyclerView.adapter = UserRecyclerAdapter(it.toList())
-        })
     }
 
     fun onGroupEditCanceled(view: View) {
@@ -81,7 +65,7 @@ class SearchGroupEditionActivity : AppCompatActivity() {
         //TODO also put other data
         searchGroupData.name = findViewById<TextView>(R.id.group_editor_group_name).text.toString()
 
-        if (!validateSearchgroupData(this, searchGroupData)){
+        if (!validateSearchgroupData(this, searchGroupData)) {
             return
         }
 
@@ -95,8 +79,8 @@ class SearchGroupEditionActivity : AppCompatActivity() {
     }
 
     companion object {
-        private fun validateSearchgroupData(searchGroupEditionActivity: SearchGroupEditionActivity, searchGroupData: SearchGroupData): Boolean{
-            if (searchGroupData.name == ""){
+        private fun validateSearchgroupData(searchGroupEditionActivity: SearchGroupEditionActivity, searchGroupData: SearchGroupData): Boolean {
+            if (searchGroupData.name == "") {
                 Toast.makeText(
                         searchGroupEditionActivity,
                         searchGroupEditionActivity.getString(R.string.search_group_edition_group_name_cannot_be_empty),
