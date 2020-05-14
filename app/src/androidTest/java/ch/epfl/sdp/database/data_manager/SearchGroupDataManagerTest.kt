e9d09467ea9486e0430093d459759a4eb860c5c0
Bastien Wermeille <5320541+Ph0tonic@users.noreply.github.com>
2020-05-14 22:23:20 +0200

    Create basic file

---
+++ app/src/androidTest/java/ch/epfl/sdp/database/data_manager/SearchGroupDataManagerTest.kt
@@ -0,0 +1,16 @@
+package ch.epfl.sdp.database.data_manager
+
+class SearchGroupDataManagerTest {
+
+//    fun deleteSearchGroup(searchGroupId: String)
+//    fun createSearchGroup(name: String): String
+//    fun getAllGroups(): MutableLiveData<List<SearchGroupData>>
+//    fun getGroupById(groupId: String): MutableLiveData<SearchGroupData>
+//    fun editGroup(searchGroupData: SearchGroupData)
+//    fun getOperatorsOfSearchGroup(searchGroupId: String): MutableLiveData<Set<UserData>>
+//    fun getRescuersOfSearchGroup(searchGroupId: String): MutableLiveData<Set<UserData>>
+//    fun removeUserOfSearchGroup(searchGroupId: String, userId: String)
+//    fun addUserToSearchgroup(searchGroupId: String, email: String, role: Role)
+
+
+}
