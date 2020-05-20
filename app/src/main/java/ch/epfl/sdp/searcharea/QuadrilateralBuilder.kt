4e5accbb33d25cdf442da79eaa6a0fc6004cb824
unknown <quentin.bernet@bluewin.ch>
2020-05-20 19:47:56 +0200

    Refactor QuadrilateralBuilder's orderVertices to use library

--- app/src/main/java/ch/epfl/sdp/searcharea/QuadrilateralBuilder.kt
+++ app/src/main/java/ch/epfl/sdp/searcharea/QuadrilateralBuilder.kt
@@ -2,6 +2,7 @@ package ch.epfl.sdp.searcharea
 
 import ch.epfl.sdp.utils.IntersectionUtils
 import com.mapbox.mapboxsdk.geometry.LatLng
+import net.mastrgamr.mbmapboxutils.SphericalUtil
 import java.util.*
 
 class QuadrilateralBuilder : SearchAreaBuilder() {
@@ -12,16 +13,8 @@ class QuadrilateralBuilder : SearchAreaBuilder() {
     override fun orderVertices() {
         if (isComplete()) {
             val data = vertices
-            fun intersect() = IntersectionUtils.doIntersect(data[0], data[2], data[1], data[3])
-
-            // Diagonals should intersect
-            if (!intersect()) {
-                Collections.swap(data, 1, 2)
-                if (!intersect()) {
-                    Collections.swap(data, 1, 2)
-                    Collections.swap(data, 2, 3)
-                }
-            }
+            val first = data[0]
+            data.sortBy { SphericalUtil.computeHeading(first, it) }
         }
     }
 
