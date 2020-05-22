47a5b54b3abe36850a736478b53f03c310f021a2
Bastien Wermeille <5320541+Ph0tonic@users.noreply.github.com>
2020-05-22 21:23:12 +0200

    Clean cirrus warnings
    Co-authored-by: Dragoo417 <17319255+Dragoo417@users.noreply.github.com>

--- app/src/main/java/ch/epfl/sdp/ui/MainActivity.kt
+++ app/src/main/java/ch/epfl/sdp/ui/MainActivity.kt
@@ -50,8 +50,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //setSupportActionBar(findViewById(R.id.toolbar))
-
         configureNavigationView()
         loadActiveGroupFromPrefs()
     }
@@ -62,13 +60,7 @@ class MainActivity : AppCompatActivity() {
         snackbar = Snackbar.make(navView, R.string.not_connected_message, Snackbar.LENGTH_LONG)
                 .setBackgroundTint(Color.BLACK).setTextColor(Color.WHITE)
 
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-//        appBarConfiguration = AppBarConfiguration(
-//                setOf(R.id.nav_home, R.id.nav_maps_managing, R.id.nav_signout_button),
-//                drawerLayout)
         val navController = findNavController(R.id.nav_host_fragment)
-//        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
         Auth.loggedIn.observe(this, Observer {
