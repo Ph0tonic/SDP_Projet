5f0f1a9e2104a3aa9369bec202d37345da485d84
unknown <quentin.bernet@bluewin.ch>
2020-06-02 15:45:52 +0200

    Move and Fix mission tests

--- app/src/test/java/ch/epfl/sdp/mission/SimpleQuadStrategyTest.kt
+++ app/src/test/java/ch/epfl/sdp/mission/SimpleQuadStrategyTest.kt
@@ -1,23 +1,15 @@
 package ch.epfl.sdp.mission
 
-import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import ch.epfl.sdp.searcharea.QuadrilateralArea
 import com.mapbox.mapboxsdk.geometry.LatLng
 import org.hamcrest.CoreMatchers.equalTo
-import org.hamcrest.MatcherAssert.assertThat
 import net.mastrgamr.mbmapboxutils.SphericalUtil.*
-import org.junit.Rule
+import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Test
-import org.junit.runner.RunWith
 import java.lang.Double.max
+import kotlin.math.abs
 
-@RunWith(AndroidJUnit4::class)
 class SimpleQuadStrategyTest {
-
-    @get:Rule
-    val instantTaskExecutorRule = InstantTaskExecutorRule()
-
     @Test(expected = IllegalArgumentException::class)
     fun doesNotAcceptNegativeMaxDistance() {
         SimpleQuadStrategy(-10.0)
@@ -50,18 +42,20 @@ class SimpleQuadStrategyTest {
 
     @Test
     fun computeMaxDistOutputsMaximumDistanceHorizontal() {
-        val start_side1 = LatLng(0.0, 0.0)
-        val start_side2 = LatLng(1.0, 1.0)
+        val startSide1 = LatLng(0.0, 0.0)
+        val startSide2 = LatLng(1.0, 1.0)
         val dist1 = 10.0
         val dist2 = 20.0
         val waypoints = listOf<LatLng>(
-                start_side1,
-                computeOffset(start_side1, dist1, 19.0),
-                start_side2,
-                computeOffset(start_side2, dist2, 75.0)
+                startSide1,
+                computeOffset(startSide1, dist1, 19.0),
+                startSide2,
+                computeOffset(startSide2, dist2, 75.0)
         )
         val res = SimpleQuadStrategy.computeMaxDist(waypoints, SimpleQuadStrategy.Orientation.HORIZONTAL)
-        assertThat(res, equalTo(max(dist1, dist2)))
+        val theoryRes = max(dist1, dist2)
+        val diff = abs(res - theoryRes)
+        assertThat("Wanted $theoryRes, but got: $res", diff < 0.01)
     }
 
     @Test
@@ -73,11 +67,13 @@ class SimpleQuadStrategyTest {
         val waypoints = listOf<LatLng>(
                 startSide1,
                 startSide2,
-                computeOffset(startSide1, dist2, 75.0),
-                computeOffset(startSide2, dist1, 19.0)
+                computeOffset(startSide2, dist1, 19.0),
+                computeOffset(startSide1, dist2, 75.0)
         )
         val res = SimpleQuadStrategy.computeMaxDist(waypoints, SimpleQuadStrategy.Orientation.VERTICAL)
-        assertThat(res, equalTo(max(dist1, dist2)))
+        val theoryRes = max(dist1, dist2)
+        val diff = abs(res-theoryRes)
+        assertThat("Wanted $theoryRes, but got: $res", diff < 0.01)
     }
 
     //TODO test that starting location is taken into account.
