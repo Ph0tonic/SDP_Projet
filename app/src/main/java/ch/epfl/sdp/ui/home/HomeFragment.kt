2538963be9ef254b3c676af90e229a2efd84d1c5
Nolan Chappuis <nolan.chappuis@epfl.ch>
2020-06-03 17:54:03 +0200

    GroupButton now shows the groupname

--- app/src/main/java/ch/epfl/sdp/ui/home/HomeFragment.kt
+++ app/src/main/java/ch/epfl/sdp/ui/home/HomeFragment.kt
@@ -4,14 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.preference.PreferenceManager
+import ch.epfl.sdp.MainApplication
 import ch.epfl.sdp.R
+import ch.epfl.sdp.database.data_manager.SearchGroupDataManager
 
 
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
+    private lateinit var groupButton: Button
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -19,6 +24,25 @@ class HomeFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
         homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        groupButton = view.findViewById(R.id.search_group_selection_button)
+        return view
+    }
+
+    override fun onResume() {
+        super.onResume()
+        updateGroupText()
+    }
+
+    private fun updateGroupText(){
+        val groupId = PreferenceManager
+                .getDefaultSharedPreferences(MainApplication.applicationContext())
+                .getString(MainApplication.applicationContext().getString(R.string.pref_key_current_group_id), null)
+        if (groupId!=null) {
+            SearchGroupDataManager().getGroupById(groupId.toString()).observeForever { group ->
+                groupButton.text = group.name
+            }
+        }
+
     }
 }
