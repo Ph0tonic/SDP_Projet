c7ae4d86cb57b08b29febb428dd7c02fd221461e
Wermeille Bastien <bastien.wermeille@gmail.com>
2021-02-23 21:24:41 +0100

    finish upgrade dependencies

--- app/src/main/java/ch/epfl/sdp/ui/maps/offline/MapsManagingFragment.kt
+++ app/src/main/java/ch/epfl/sdp/ui/maps/offline/MapsManagingFragment.kt
@@ -9,43 +9,46 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import ch.epfl.sdp.MainApplication
 import ch.epfl.sdp.R
 import ch.epfl.sdp.map.offline.OfflineRegionUtils
 import ch.epfl.sdp.utils.OnItemClickListener
 import com.mapbox.mapboxsdk.offline.OfflineManager
 import com.mapbox.mapboxsdk.offline.OfflineRegion
-import kotlinx.android.synthetic.main.fragment_maps_managing.*
 
 class MapsManagingFragment : Fragment() {
 
+    private lateinit var recyclerView: RecyclerView
     private lateinit var mapsManagingViewModel: MapsManagingViewModel
     private lateinit var offlineManager: OfflineManager
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         mapsManagingViewModel = ViewModelProvider(this).get(MapsManagingViewModel::class.java)
         offlineManager = OfflineManager.getInstance(MainApplication.applicationContext())
-        return inflater.inflate(R.layout.fragment_maps_managing, container, false)
+        val view :View = inflater.inflate(R.layout.fragment_maps_managing, container, false)
+        recyclerView = view.findViewById(R.id.mapSelectionRecyclerview)
+        return view
     }
 
     override fun onStart() {
         super.onStart()
         offlineManager.listOfflineRegions(object : OfflineManager.ListOfflineRegionsCallback {
             override fun onList(offlineRegions: Array<OfflineRegion>) { // Check result. If no regions have been
+
                 // RecyclerView node initialized here
-                mapSelectionRecyclerview.apply {
+                recyclerView.apply {
                     // set a LinearLayoutManager to handle Android
                     // RecyclerView behavior
                     layoutManager = LinearLayoutManager(activity)
                     // set the custom adapter to the RecyclerView
                     adapter = MapSelectionRecyclerViewAdapter(offlineRegions, object : OnItemClickListener<OfflineRegion> {
-                        override fun onItemClicked(offlineRegion: OfflineRegion) {
-                            openExistingRegion(offlineRegion)
+                        override fun onItemClicked(item: OfflineRegion) {
+                            openExistingRegion(item)
                         }
-
                     })
                 }
                 view?.findViewById<TextView>(R.id.no_offline_map)?.visibility = if (offlineRegions.isNotEmpty()) {
